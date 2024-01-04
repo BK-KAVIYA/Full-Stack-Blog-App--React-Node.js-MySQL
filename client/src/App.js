459ab1716d725b543c32c34fb5baa42899010683
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-01-04 12:29:46 +0530

    all update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React from 'react';
+import {createBrowserRouter,RouterProvider,Route, Outlet} from 'react-router-dom';
+import  Register  from './pages/Register';
+import  Login  from './pages/Login';
+import  Home  from './pages/Home';
+import  Single  from './pages/Single';
+import  Write  from './pages/Write';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import "./style.scss";
+
+
+const Layout=()=>{
+  return(
+    <div>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children:[
+      {
+        path:'/',
+        element:<Home/>,
+      },
+      {
+        path:'/post/:Id',
+        element:<Single/>,
+      },
+      {
+        path:'/write',
+        element:<Write/>,
+      },
+    ]
+  },
+  {
+    path:'/register',
+    element:<Register/>,
+  },
+  {
+    path:'/login',
+    element:<Login/>,
+  }
+  
+]);
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router}/> 
+      </div>
+    </div>
+  );
 }
+
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='auth'>
+        <h1>Login</h1>
+        <form>
+            <input type='text' placeholder='username' />
+            <input type='password' placeholder='password' />
+            <button>Login</button>
+        </form>
+    </div>
+  )
+}

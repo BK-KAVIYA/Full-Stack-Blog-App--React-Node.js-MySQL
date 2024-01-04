51669168c82d2c1caaedbe11f1a6536863dc87b5
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-01-04 13:26:45 +0530

    login and register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Navigate } from 'react-router-dom'
 
 export default function Login() {
   return (
@@ -8,6 +9,8 @@ export default function Login() {
             <input type='text' placeholder='username' />
             <input type='password' placeholder='password' />
             <button>Login</button>
+            <p>This is an error!</p>
+            <span>Don't have account? <Link to="/register">REGISTER</Link></span>
         </form>
     </div>
   )
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link, Navigate } from 'react-router-dom'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className='auth'>
+        <h1>Login</h1>
+        <form>
+            <input type='text' placeholder='username' required/>
+            <input type='email' placeholder='email' required/>
+            <input type='password' placeholder='password' required/>
+            <button>Login</button>
+            <p>This is an error!</p>
+            <span>Do yoy have account? <Link to="/login">LOGIN</Link></span>
+        </form>
+    </div>
   )
 }
-
